18512c6c47c0681b8b851dba1db910bea7755877
Maxwell Joslyn <maxwelljoslyn@gmail.com>
2024-04-21 16:50:06 -0700

    update question 4 answer for revised requirements

--- q4.sql
+++ q4.sql
@@ -1,8 +1,8 @@
 select
 salesperson.name as salesperson_name,
-coalesce(sum(orders.amount), 0) as total_sales_amount
+coalesce(sum(orders.amount), 0) / cast(salesperson.salary as real) as performance
 from
 salesperson
 left join orders on orders.salesperson_id=salesperson.id
 group by salesperson.id
-order by total_sales_amount desc;
+order by performance desc;
